d21f0fa8b288fbe861e79278d4dc4a9e66368dd5
Marcelo Lauxen <marcelolauxen16@gmail.com>
2021-05-19 21:55:37 -0300

    Test calling renderStreamMessage manually

--- app/javascript/controllers/broker_account_controller.js
+++ app/javascript/controllers/broker_account_controller.js
@@ -8,4 +8,12 @@ export default class extends Controller {
     const broker_broker_account_id = this.editing_broker_accountTarget.dataset.brokerAccountId
     event.target.href = `${event.target.dataset.editPath}?editing_broker_account_id=${broker_broker_account_id}`
   }
+
+  crazy_action(event) {
+    event.preventDefault()
+
+    const message = `<turbo-stream action="remove" target="broker_account_12"></turbo-stream>`
+
+    Turbo.renderStreamMessage(message)
+  }
 }
