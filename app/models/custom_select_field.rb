d98abbd76bb087298b052a42c0edf0769c19f4e9
Marcelo Lauxen <marcelolauxen16@gmail.com>
2021-05-31 21:37:32 -0300

    Adds validation to not allow a custom select field be saved with duplicated choices

--- app/models/custom_select_field.rb
+++ app/models/custom_select_field.rb
@@ -2,4 +2,14 @@ class CustomSelectField < ApplicationRecord
   attribute :choices, :string, default: ''
 
   validates :name, :choices, presence: true
+  validate :choices_uniqueness
+
+  private
+
+  def choices_uniqueness
+    choices_list = choices.split(";")
+    return if choices_list.size == choices_list.uniq.size
+
+    errors.add(:choices, "are not allowed to be duplicated")
+  end
 end
